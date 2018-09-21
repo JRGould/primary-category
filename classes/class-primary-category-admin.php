48a97388e215aecc5164fb24df08b95c9e034940
Jeff Gould <jrgould@gmail.com>
2018-09-20 22:50:16 -0700

    documentation, reorganization, cleanup

--- classes/class-primary-category-admin.php
+++ classes/class-primary-category-admin.php
@@ -7,23 +7,28 @@
  */
 class Primary_Category_Admin {
 
-	public $config;
+	protected $config;
 
-	function __construct( $config ) {
+	public function __construct( $config ) {
 		$this->config = $config;
 	}
 
-	function init() {
+	/**
+	 * Init class specifically rather than when constructed.
+	 */
+	public function init() {
 		if ( ! $this->should_init() ) {
 			return;
 		}
 
 		add_action( 'admin_enqueue_scripts', array( $this, 'enqueue_scripts' ) );
-
 		add_action( 'save_post', array( $this, 'save_primary_category' ) );
 	}
 
-	function enqueue_scripts() {
+	/**
+	 * Enqueues styles and scripts including localizations / php values to be passed to js.
+	 */
+	public function enqueue_scripts() {
 		wp_register_script( 'primary-category-admin-script', $this->config['base_url'] . '/assets/primary-category.js', 'JQuery', $this->config['version'], true );
 
 		wp_localize_script( 'primary-category-admin-script', 'primary_category_data', array(
@@ -38,14 +43,12 @@ class Primary_Category_Admin {
 		wp_enqueue_style( 'primary-category-admin-style' );
 	}
 
-	private function get_js_localized_strings() {
-		return array(
-			'primary'      => _x( 'Primary', 'main or principal', $this->config['text_domain'] ),
-			'make primary' => _x( 'Make Primary', 'designate category as primary category', $this->config['text_domain'] ),
-		);
-	}
-
-	private function should_init() {
+	/**
+	 * Determines if this class should be allowed to initialize itself, prevents loading assets where not needed.
+	 *
+	 * @return bool
+	 */
+	protected function should_init() {
 		global $pagenow;
 		if ( in_array( $pagenow, array( 'edit.php', 'post.php' ) ) ) {
 			return true;
@@ -54,17 +57,10 @@ class Primary_Category_Admin {
 		return false;
 	}
 
-	private function get_current_post_primary_category() {
-		global $post;
-		if ( ! $post || ! $post->ID ) {
-			return 0;
-		}
-		$primary = get_post_meta( $post->ID, '_primary-category', true );
-
-		return $primary;
-	}
-
-	function save_primary_category() {
+	/**
+	 * Saves the primary category from posted data for the current post.
+	 */
+	public function save_primary_category() {
 		global $post;
 
 		check_admin_referer( 'save-primary-category-field', '_primary-category_nonce' );
@@ -77,4 +73,31 @@ class Primary_Category_Admin {
 		update_post_meta( $post->ID, '_primary-category', $primary_category );
 	}
 
+	/**
+	 * Assembles translations for js strings.
+	 *
+	 * @return array
+	 */
+	protected function get_js_localized_strings() {
+		return array(
+			'primary'      => _x( 'Primary', 'main or principal', $this->config['text_domain'] ),
+			'make primary' => _x( 'Make Primary', 'designate category as primary category', $this->config['text_domain'] ),
+		);
+	}
+
+
+	/**
+	 * Gets the primary category for the current global $post, returns 0 if not specified or available.
+	 *
+	 * @return int
+	 */
+	protected function get_current_post_primary_category() {
+		global $post;
+		if ( ! $post || ! $post->ID ) {
+			return 0;
+		}
+		$primary = get_post_meta( $post->ID, '_primary-category', true );
+
+		return $primary;
+	}
 }
